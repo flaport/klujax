d81aa1d9077b9da47251ab9dce91ab29a6178138
flaport <floris.laporte@gmail.com>
2021-06-02 18:39:30 +0200

    stub xla translation

--- klujax.py
+++ klujax.py
@@ -14,6 +14,14 @@ def solve_abstract_eval(A, b):
 
 solve_prim.def_abstract_eval(solve_abstract_eval)
 
+
+## This should be replaced by c++-registration...
+from jax.lib import xla_client
+def solve_xla_translation(c, Ac, bc):
+    return xla_client.ops.Div(bc, Ac)
+from jax.interpreters import xla
+xla.backend_specific_translations['cpu'][solve_prim] = solve_xla_translation
+
 if __name__ == "__main__":
     print(solve(3.0, 4.0))
     print(jax.jit(solve)(3.0, 4.0))
