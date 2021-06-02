4496ac879433ef2434ad86be4c29ad2e8084c80c
flaport <floris.laporte@gmail.com>
2021-06-02 23:48:41 +0200

    make it work for 1D arrays

--- klujax.cpp
+++ klujax.cpp
@@ -3,13 +3,21 @@
 namespace py = pybind11;
 
 void solve_f32(void* out, void** in) {
-  float A = ((float*) in[0])[0];
-  float b = ((float*) in[1])[0];
+  float* A = ((float*) in[0]);
+  float* b = ((float*) in[1]);
   float* result = (float*) out;
-  result[0] = b / A;
+  int length = sizeof(&b) / sizeof(b[0]);
+  for (int i=0; i<length; i++){
+    result[i] = b[i] / A[i];
+  }
 }
 
-
 PYBIND11_MODULE(klujax_cpp, m) {
-    m.def("solve_f32", [](){ const char* name = "xla._CUSTOM_CALL_TARGET"; return py::capsule((void *) &solve_f32, name);}, "solve a linear system of equations");
+  m.def(
+      "solve_f32",
+      []() {
+        const char *name = "xla._CUSTOM_CALL_TARGET";
+        return py::capsule((void *)&solve_f32, name);
+      },
+      "solve a linear system of equations");
 }
--- klujax.py
+++ klujax.py
@@ -7,28 +7,35 @@ import klujax_cpp
 
 solve_prim = jax.core.Primitive("solve")
 
+
 def solve(A, b):
     return solve_prim.bind(A, b)
 
+
 def solve_impl(A, b):
-    return b/A
+    return b / A
+
 
 solve_prim.def_impl(solve_impl)
 
+
 def solve_abstract_eval(A, b):
     return jax.abstract_arrays.ShapedArray(A.shape, A.dtype)
+
 
 solve_prim.def_abstract_eval(solve_abstract_eval)
 
 
 # make jittable
-# This should be replaced by c++-registration...
 from jax.lib import xla_client
 from jax.interpreters import xla
+
 xla_client.register_cpu_custom_call_target(
     b"solve",
     klujax_cpp.solve_f32(),
 )
+
+
 def solve_xla_translation(c, Ac, bc):
     shape = c.get_shape(Ac)
     return xla_client.ops.CustomCallWithLayout(
@@ -42,35 +49,34 @@ def solve_xla_translation(c, Ac, bc):
         shape_with_layout=shape,
     )
 
-xla.backend_specific_translations['cpu'][solve_prim] = solve_xla_translation
 
+xla.backend_specific_translations["cpu"][solve_prim] = solve_xla_translation
 
 
 # make differentiable
-from jax.interpreters import ad
-def solve_value_and_jvp(arg_values, arg_tangents):
-    A, b = arg_values
-    At, bt = arg_tangents
-    primal_out = solve(A, b)
-
-    At = jnp.zeros_like(A) if isinstance(At, ad.Zero) else At
-    bt = jnp.zeros_like(b) if isinstance(bt, ad.Zero) else bt
-    output_tan = -b*At/A**2 + bt/A
-    return primal_out, output_tan
-
-ad.primitive_jvps[solve_prim] = solve_value_and_jvp
+# we'll work on this later...
+#
+# from jax.interpreters import ad
+#
+#
+# def solve_value_and_jvp(arg_values, arg_tangents):
+#     A, b = arg_values
+#     At, bt = arg_tangents
+#     primal_out = solve(A, b)
+#
+#     At = jnp.zeros_like(A) if isinstance(At, ad.Zero) else At
+#     bt = jnp.zeros_like(b) if isinstance(bt, ad.Zero) else bt
+#     output_tan = -b * At / A ** 2 + bt / A
+#     return primal_out, output_tan
+#
+# ad.primitive_jvps[solve_prim] = solve_value_and_jvp
 
 if __name__ == "__main__":
-    a = jnp.array(3.0)
-    b = jnp.array(4.0)
+    a = jnp.array([3.0, 5.0])
+    b = jnp.array([7.0, 11.0])
     t1 = time()
     print(solve(a, b))
     t0, t1 = t1, time()
-    print(t1-t0)
+    print(t1 - t0)
     print(jax.jit(solve)(a, b))
     t0, t1 = t1, time()
-    print(t1-t0)
-    print(jax.grad(solve)(a, b))
-    t0, t1 = t1, time()
-    print(t1-t0)
-
