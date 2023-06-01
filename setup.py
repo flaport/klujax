1b1167fa004f017f8ca22cea27b866d5789420c0
flaport <floris.laporte@gmail.com>
2023-06-01 10:39:31 -0700

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ klujax_cpp = Extension(
 
 setup(
     name="klujax",
-    version="0.1.3",
+    version="0.1.4",
     author="Floris Laporte",
     author_email="floris.laporte@gmail.com",
     description="a KLU solver for JAX",
