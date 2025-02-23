3e194562d710b584890626e5dff77ef010b11316
flaport <floris.laporte@gmail.com>
2025-02-23 18:03:39 +0100

    cpp: add dot_c128

--- klujax.cpp
+++ klujax.cpp
@@ -12,20 +12,17 @@ namespace ffi = xla::ffi;
 #include <cstring>  // for memset
 
 ffi::Error validate_dot_f64_args(
-    const ffi::Buffer<ffi::DataType::S32> &Ai,
-    const ffi::Buffer<ffi::DataType::S32> &Aj,
-    const ffi::Buffer<ffi::DataType::F64> &Ax,
-    const ffi::Buffer<ffi::DataType::F64> &x) {
-    auto ds_x = x.dimensions();
+    ffi::Buffer<ffi::DataType::S32> &Ai,
+    ffi::Buffer<ffi::DataType::S32> &Aj,
+    const ffi::AnyBuffer::Dimensions ds_Ax,
+    const ffi::AnyBuffer::Dimensions ds_x) {
     int d_x = ds_x.size();
     if (d_x != 3) {
         return ffi::Error::InvalidArgument("x is not 3D.");
     }
     int n_lhs = (int)ds_x[0];
     int n_col = (int)ds_x[1];
-    int n_rhs = (int)ds_x[2];
 
-    auto ds_Ax = Ax.dimensions();
     int d_Ax = ds_Ax.size();
     if (d_Ax != 2) {
         return ffi::Error::InvalidArgument("Ax is not 2D.");
@@ -83,23 +80,22 @@ ffi::Error _dot_f64(
     ffi::Buffer<ffi::DataType::F64> Ax,
     ffi::Buffer<ffi::DataType::F64> x,
     ffi::Result<ffi::Buffer<ffi::DataType::F64>> b) {
-    ffi::Error err = validate_dot_f64_args(Ai, Aj, Ax, x);
+    auto ds_x = x.dimensions();
+    auto ds_Ax = Ax.dimensions();
+    ffi::Error err = validate_dot_f64_args(Ai, Aj, ds_Ax, ds_x);
     if (err.failure()) {
         return err;
     }
 
+    int n_lhs = (int)ds_x[0];
+    int n_col = (int)ds_x[1];
+    int n_rhs = (int)ds_x[2];
+    int n_nz = (int)ds_Ax[1];
     const int *_Ai = Ai.typed_data();
     const int *_Aj = Aj.typed_data();
     const double *_Ax = Ax.typed_data();
     const double *_x = x.typed_data();
     double *_b = b->typed_data();
-
-    auto ds_x = x.dimensions();
-    auto ds_Ax = Ax.dimensions();
-    int n_lhs = (int)ds_x[0];
-    int n_col = (int)ds_x[1];
-    int n_rhs = (int)ds_x[2];
-    int n_nz = (int)ds_Ax[1];
 
     // initialize empty result
     for (int i = 0; i < n_lhs * n_col * n_rhs; i++) {
@@ -133,8 +129,70 @@ XLA_FFI_DEFINE_HANDLER_SYMBOL(  // b = A x
         .Ret<ffi::Buffer<ffi::DataType::F64>>()  // b
 );
 
+ffi::Error _dot_c128(
+    ffi::Buffer<ffi::DataType::S32> Ai,
+    ffi::Buffer<ffi::DataType::S32> Aj,
+    ffi::Buffer<ffi::DataType::C128> Ax,
+    ffi::Buffer<ffi::DataType::C128> x,
+    ffi::Result<ffi::Buffer<ffi::DataType::C128>> b) {
+    auto ds_x = x.dimensions();
+    auto ds_Ax = Ax.dimensions();
+    ffi::Error err = validate_dot_f64_args(Ai, Aj, ds_Ax, ds_x);
+    if (err.failure()) {
+        return err;
+    }
+    int n_lhs = (int)ds_x[0];
+    int n_col = (int)ds_x[1];
+    int n_rhs = (int)ds_x[2];
+    int n_nz = (int)ds_Ax[1];
+    const int *_Ai = Ai.typed_data();
+    const int *_Aj = Aj.typed_data();
+    const double *_Ax = (double *)Ax.typed_data();
+    const double *_x = (double *)x.typed_data();
+    double *_b = (double *)b->typed_data();
+
+    // initialize empty result
+    for (int i = 0; i < 2 * n_lhs * n_col * n_rhs; i++) {
+        _b[i] = 0.0;
+    }
+
+    // fill result (all multi-dim arrays are row-major)
+    // x_mik = A_mij × x_mjk (einsum)
+    // sizes: m<n_lhs; i<n_col<--Ai; j<n_col<--Aj; k<n_rhs
+    int i;
+    int j;
+    for (int n = 0; n < n_nz; n++) {
+        i = _Ai[n];
+        j = _Aj[n];
+        for (int m = 0; m < n_lhs; m++) {
+            for (int k = 0; k < n_rhs; k++) {
+                _b[2 * (m * n_col * n_rhs + i * n_rhs + k)] +=                                        // real
+                    _Ax[2 * (m * n_nz + n)] * _x[2 * (m * n_col * n_rhs + j * n_rhs + k)]             // real*real
+                    - _Ax[2 * (m * n_nz + n) + 1] * _x[2 * (m * n_col * n_rhs + j * n_rhs + k) + 1];  // imag*imag
+                _b[2 * (m * n_col * n_rhs + i * n_rhs + k) + 1] +=                                    // imag
+                    _Ax[2 * (m * n_nz + n)] * _x[2 * (m * n_col * n_rhs + j * n_rhs + k) + 1]         // real*imag
+                    + _Ax[2 * (m * n_nz + n) + 1] * _x[2 * (m * n_col * n_rhs + j * n_rhs + k)];      // imag*real
+            }
+        }
+    }
+
+    return ffi::Error::Success();
+}
+
+XLA_FFI_DEFINE_HANDLER_SYMBOL(  // b = A x
+    dot_c128, _dot_c128,
+    ffi::Ffi::Bind()
+        .Arg<ffi::Buffer<ffi::DataType::S32>>()   // Ai
+        .Arg<ffi::Buffer<ffi::DataType::S32>>()   // Aj
+        .Arg<ffi::Buffer<ffi::DataType::C128>>()  // Ax
+        .Arg<ffi::Buffer<ffi::DataType::C128>>()  // x
+        .Ret<ffi::Buffer<ffi::DataType::C128>>()  // b
+);
+
 // Python wrappers
 PYBIND11_MODULE(klujax_cpp, m) {
     m.def("dot_f64",
+          []() { return py::capsule((void *)&dot_f64); });
+    m.def("dot_c128",
           []() { return py::capsule((void *)&dot_f64); });
 }
