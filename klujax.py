d5993f63c041d86a5267758eaef184b5cee5695d
flaport <floris.laporte@gmail.com>
2025-02-24 23:22:55 +0100

    Bump to 0.4.0

--- klujax.py
+++ klujax.py
@@ -2,7 +2,7 @@
 
 # Metadata ============================================================================
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 __author__ = "Floris Laporte"
 __all__ = ["coalesce", "dot", "solve"]
 
